39413ff8e6a3e17a8c2ef971ac95642458396502
Alice Beserra <153461290+alyuz@users.noreply.github.com>
2025-11-16 03:04:22 -0300

    Adicionando .exposedDropdownMenuAnchor()

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.material3:material3-experimental:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
